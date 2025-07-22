c82e806a02ee4d66cf1bf0a0bdd0006ad7ad7c2b
LichLN <lichln@bsscommerce.com>
2025-07-22 18:04:29 +0700

    fix cross origin

--- api/gemini_generate_content.js
+++ api/gemini_generate_content.js
@@ -2,22 +2,30 @@ import fetch from "node-fetch";
 import { GoogleGenAI } from "@google/genai";
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+  res.setHeader('Access-Control-Allow-Origin', 'https://nhat-memo.pages.dev');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
   }
-  try {
-    const { query } = req.body;
-    const geminiApiKey = process.env.GEMINI_API_KEY;
-    const ai = new GoogleGenAI({
-        apiKey: geminiApiKey
-    });
-    const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
-        contents: query,
-    });
-    res.status(200).json({'text': response.text});
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ error: e.message });
+
+  if (req.method === 'POST') {
+    try {
+      const { query } = req.body;
+      const geminiApiKey = process.env.GEMINI_API_KEY;
+      const ai = new GoogleGenAI({
+          apiKey: geminiApiKey
+      });
+      const response = await ai.models.generateContent({
+          model: "gemini-2.5-flash",
+          contents: query,
+      });
+      res.status(200).json({'text': response.text});
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ error: e.message });
+    }
   }
+  return res.status(405).json({ error: 'Method not allowed' });
 }
